e528f13ae1d574e80f2257591f2ebd0c22225f2b
Joe Constant <joe@joeconstant.com>
2023-04-19 20:52:34 -0600

    Add home page content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,19 @@
 export default function HomePage() {
-  return <h1>Home</h1>
+  return (
+    <article className="container">
+      <header>Welcome to NASA Image & Video Search site</header>
+      <section>
+        This site will allow you to search the NASA image and video archives
+        based on a keyword
+      </section>
+      <section>
+        To get started, use the navigation bar above and choose either "Image
+        Search" or "Video Search"
+      </section>
+      <section>
+        If you want to see a list of your previous searches during this browsing
+        session, please visit the "History" page
+      </section>
+    </article>
+  )
 }
